c641a6265ad01e54696dae3a992face708294743
Caroline Riggall <criggall@lleeimac2021wireless.nomad.utk.edu>
2025-04-17 17:29:36 -0400

    Working on fit

---
+++ Simplified-HFOFO/fit.py
@@ -0,0 +1,93 @@
+import numpy as np
+from scipy.interpolate import interp1d
+from scipy.optimize import curve_fit
+from matplotlib import pyplot as plt
+
+# Length of period:
+len_period = 4.2 # m
+
+# Absolute path to present directory:
+main_dir = '/Users/criggall/Documents/muon-cooling/Simplified-HFOFO/'
+
+# Load data for offset particle:
+file = main_dir+'offset_particle_output/AllTracks_dp_neg_dxy_extended.txt'
+data = np.loadtxt(file)
+
+# Load data for reference particle:
+file_ref = main_dir+'offset_particle_output/AllTracks_nominal_extended.txt'
+data_ref = np.loadtxt(file_ref)
+
+# Values along channel:
+x_vals = []; y_vals = []; z_vals = []; t_vals = []
+x_vals_ref = []; y_vals_ref = []; z_vals_ref = []; t_vals_ref = []
+next_z = 0.5; period_start_z_vals = []; period_start_indices = [] # start solenoid placement at 0.5 m
+for i in range(data.shape[0]):
+    x_vals.append(data[i][0]*0.1) # mm -> cm
+    y_vals.append(data[i][1]*0.1)
+    z = data[i][2]*0.001 # mm -> m
+    z_vals.append(z)
+    t_vals.append(data[i][6]) # ns
+    x_vals_ref.append(data_ref[i][0]*0.1) # mm -> cm
+    y_vals_ref.append(data_ref[i][1]*0.1)
+    z_ref = data_ref[i][2]*0.001 # mm -> m
+    z_vals_ref.append(z_ref)
+    t_vals_ref.append(data_ref[i][6]) # ns
+
+    # Find indices for first value in each period:
+    if z > next_z:
+        period_start_z_vals.append(z)
+        period_start_indices.append(i)
+        next_z += len_period
+
+# Remove last period -- just for reducing end field effects:
+start_last_period = period_start_indices[len(period_start_indices)-1]
+x_vals = x_vals[:start_last_period]
+y_vals = y_vals[:start_last_period]
+z_vals = z_vals[:start_last_period]
+t_vals = t_vals[:start_last_period]
+x_vals_ref = x_vals_ref[:start_last_period]
+y_vals_ref = y_vals_ref[:start_last_period]
+z_vals_ref = z_vals_ref[:start_last_period]
+t_vals_ref = t_vals_ref[:start_last_period]
+
+# Find displacement:
+displacements = []
+for i in range(len(z_vals)):
+    displacements.append(np.sqrt( (x_vals_ref[i] - x_vals[i])**2 + (y_vals_ref[i] - y_vals[i])**2 ))
+
+# Evenly space z values:
+z_uniform = np.linspace(np.min(z_vals), np.max(z_vals), len(z_vals))
+displacements_interp_func = interp1d(z_vals, displacements)
+displacements_uniform = displacements_interp_func(z_uniform)
+
+# Perform FFT:
+fft = np.fft.fft(displacements_uniform)
+dz = z_uniform[1] - z_uniform[0]
+wave_nums = np.fft.fftfreq(len(z_uniform), d=dz)
+amplitude = np.abs(fft)
+wave_nums = wave_nums[:len(wave_nums)//2]
+amplitude = amplitude[:len(amplitude)//2]
+
+# Amplitude and wave number values from FFT:
+a = [0.25, 0.05, 0.08]
+k = [0.55, 0.77, 1.43]
+
+# Define functional form of observed dp particle trajectory:
+# def f(z, phi0, phi1, phi2):
+#     return ( a[0]*np.sin(k[0]*z+phi0) +  a[1]*np.sin(k[1]*z+phi1) + a[2]*np.sin(k[2]*z+phi2))
+def f(z):
+    return ( a[0]*np.sin(k[0]*z) +  a[1]*np.sin(k[1]*z) + a[2]*np.sin(k[2]*z))
+
+# Fit to find phases:
+initial_phases = [0, 0, 0]
+# popt, pcov = curve_fit(f, z_uniform, displacements_uniform, p0=initial_phases)
+
+# Plot:
+plt.figure(figsize=(12,5))
+plt.plot(z_uniform, displacements_uniform, label='data',color='black')
+# plt.plot(z_uniform, f(z, *popt), label='fit', linestyle='--',color='green')
+plt.plot(z_uniform, f(z_uniform), label='fit', linestyle='--',color='green')
+plt.xlabel('z (m)')
+plt.ylabel('displacement (cm)')
+plt.legend()
+plt.savefig(main_dir+'Figures/fit_test.png')
