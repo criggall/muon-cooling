f760ad2e0a8b3ecfc2f05affe024b57ba429f4f6
Caroline Riggall <criggall@vols.utk.edu>
2025-07-14 13:01:30 -0500

    Standardized plotting style

---
+++ functions/set_plot_settings.py
@@ -0,0 +1,23 @@
+import os
+from matplotlib import pyplot as plt
+
+
+def setPlotSettings(font=False):
+
+    ''' Set font and dpi for plotting.
+    
+    Inputs:
+    font = Boolean, whether to set font to LaTeX -- requires LaTeX installation! '''
+    
+    # Set dpi:
+    plt.rcParams['figure.dpi'] = 300
+
+    # Set LaTeX font:
+    if font == True:
+        os.environ["PATH"] = "/Library/TeX/texbin:" + os.environ["PATH"] # path to LaTeX installation
+        plt.rcParams.update({
+            "text.usetex": True,
+            "font.family": "serif",
+            "font.serif": ["Computer Modern"],
+            "text.latex.preamble": r"\usepackage{amsmath}"
+        })
